f21cafc996064c890b6e0401f345e25b53fe876b
orellabac <orellabac>
2023-01-06 05:18:03 +0000

    some fixes still two test failing

--- src/main/scala/com/snowflake/snowpark_extensions/Extensions.scala
+++ src/main/scala/com/snowflake/snowpark_extensions/Extensions.scala
@@ -9,9 +9,11 @@ package com.snowflake.snowpark_extensions
 
 import com.snowflake.snowpark.Column
 import com.snowflake.snowpark.DataFrame
+import com.snowflake.snowpark.CaseExpr
 import com.snowflake.snowpark_extensions.implicits.ColumnExtensions
 import com.snowflake.snowpark_extensions.implicits.DataFrameExtensions
 import com.snowflake.snowpark_extensions.implicits.SessionExtensions
+import com.snowflake.snowpark_extensions.implicits.CaseExprExtensions
 import com.snowflake.snowpark.Session
 
 object Extensions {
