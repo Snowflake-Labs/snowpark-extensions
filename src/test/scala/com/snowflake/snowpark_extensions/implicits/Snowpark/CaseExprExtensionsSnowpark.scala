4c34569d7f1e409f09097dfc9e7cf790f06c0a47
orellabac <orellabac>
2023-01-06 05:23:40 +0000

    solving isssues in tests

--- src/test/scala/com/snowflake/snowpark_extensions/implicits/Snowpark/CaseExprExtensionsSnowpark.scala
+++ src/test/scala/com/snowflake/snowpark_extensions/implicits/Snowpark/CaseExprExtensionsSnowpark.scala
@@ -16,7 +16,7 @@ object CaseExprExtensionsSnowpark {
 
   //Function: otherwise(value: Any): Column
   def test_otherwise() : DataFrame = {
-    df2.withColumn("colNew", when(col("COL3")==="5",lit("1")).otherwise(col("0")))//.show()
+    df2.withColumn("colNew", when(col("COL3")==="5",lit("1")).otherwise(lit("0")))
   }
 
 }
