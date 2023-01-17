89e039f142550ec4186861ab10ab08a067770674
orellabac <orellabac>
2023-01-17 04:10:18 +0000

    update when extension

--- src/test/scala/com/snowflake/snowpark_extensions/implicits/FunctionExtensionsTest.scala
+++ src/test/scala/com/snowflake/snowpark_extensions/implicits/FunctionExtensionsTest.scala
@@ -6,11 +6,15 @@ import com.snowflake.snowpark_extensions.testutils.Serializer.df2Seq
 import com.snowflake.snowpark_extensions.testutils.SessionInitializer
 import org.scalatest.{FlatSpec, Matchers}
 import com.snowflake.snowpark.Row
+import com.snowflake.snowpark.functions.{col}
 import com.snowflake.snowpark.types._
 
 class FunctionExtensionsTest extends FlatSpec with Matchers {
   behavior of "FunctionExtenions class"
 
+  "when expression" should "compile without errors" in {
+    when(col("a") > 1,"literal1").otherwise("literal2")
+  }
 
   "substring_index" should "match spark substring_index" in {
     // df = spark.createDataFrame([('a.b.c.d',)], ['s'])
@@ -28,7 +32,7 @@ class FunctionExtensionsTest extends FlatSpec with Matchers {
     Seq(Seq("a.b"))   shouldEqual df2Seq(df.select(substring_index(df("s"),".",2)))
   }
 
-    "substring_index negative index" should "match spark substring_index" in {
+  "substring_index negative index" should "match spark substring_index" in {
     // df = spark.createDataFrame([('a.b.c.d',)], ['s'])
     // df.select(substring_index(df.s, '.', 2).alias('s')).collect()
     // [Row(s='a.b')]
